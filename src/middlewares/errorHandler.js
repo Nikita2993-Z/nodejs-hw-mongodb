c30f708992e0bf298be9d447db3dceb42844cd3b
Nikita2993-Z <zinchenko.nikita93@gmail.com>
2025-08-18 17:00:32 +0300

    error correction

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,24 +1,29 @@
 import { isHttpError } from 'http-errors';
 import { MongooseError } from 'mongoose';
 
-export const errorHandlerMiddleware = (err, req, res) => {
-    if(isHttpError(err)) {
-    return res.status(err.status).json({
+export const errorHandlerMiddleware = (err, req, res, next) => {
+  if (isHttpError(err)) {
+    const payload = {
       status: err.status,
-      message: err.name,
-      data: err.message,
-    });
-}
-if(err instanceof MongooseError) {
+      message: err.expose ? err.message || 'Error' : 'Error',
+    };
+    if (err.errors || (err?.data && err.data.errors)) {
+      payload.errors = err.errors ?? err.data.errors;
+    }
+    return res.status(err.status).json(payload);
+  }
+
+  if (err instanceof MongooseError) {
     return res.status(500).json({
       status: 500,
       message: 'MongooseError',
-      data: err.message,
+      errors: [{ message: err.message }],
     });
-}
-res.status(500).json({
+  }
+
+  return res.status(500).json({
     status: 500,
-    message: "Something went wrong",
-    data: err.message,
+    message: 'Something went wrong',
+    errors: [{ message: err.message }],
   });
 };
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,8 +1,18 @@
-export const validateBody = (schema) => async (req, res, next) => {
-  await schema.validateAsync(req.body, {
-    allowUnknown: false,
+import createHttpError from 'http-errors';
+
+export const validateBody = (schema) => (req, res, next) => {
+  const { value, error } = schema.validate(req.body, {
     abortEarly: false,
-    convert: false,
+    stripUnknown: true,
   });
-  next();
+  if (!error) {
+    req.body = value;
+    return next();
+  }
+  const errors = error.details.map((d) => ({
+    path: d.path.join('.'),
+    message: d.message,
+    type: d.type,
+  }));
+  return next(createHttpError(400, { message: 'Validation error', errors }));
 };
